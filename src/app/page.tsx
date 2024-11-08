cf3789159f528477baf753d6a280fac9d363601d
emirevec <vecchioemiliano@gmail.com>
2024-11-08 11:52:44 -0300

    feat: add table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { ReceiptsTable } from "@/table";
+
 export default function Home() {
   return (
-    <h1>CommunApp</h1>
+    <>
+      <h1>CommunApp</h1>
+      <ReceiptsTable/>
+    </>
   );
 }
