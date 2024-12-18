42a59a31589ea41aa87ceedc5976471bd09a7e6e
emirevec <vecchioemiliano@gmail.com>
2024-12-18 13:43:33 -0300

    fix: correct filter order by seller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import { OrdersFilters } from "@/features/orders/components/OrdersFilters"
 import { OrdersList } from "@/features/orders/components/OrdersList"
 import { fetchWooCommerceOrdersByDate } from "@/features/orders/lib/wooCommerceApi"
+import { filterOrdersBySeller } from "@/features/orders/util/filterOrdersBySeller"
 
 export default async function Home() {
-  const after = "2024-12-07T00:00:00"
-  const before ="2024-12-11T23:59:59"
-  const orders = await fetchWooCommerceOrdersByDate({after, before})
+  const after = "2024-12-16T00:00:00"
+  const before ="2024-12-18T23:59:59"
+  const seller = 1
+  const ordersFromWooCommerce = await fetchWooCommerceOrdersByDate({after, before})
+  const orders = filterOrdersBySeller({orders: ordersFromWooCommerce, seller})
+
   return (
     <>
       <h1>CommunApp</h1>
       <OrdersFilters/>
-      {/* <OrdersList orders={orders}/> */}
+      <OrdersList orders={orders}/>
     </>
   )
 }
--- src/features/orders/util/filterOrdersBySeller.ts
+++ src/features/orders/util/filterOrdersBySeller.ts
@@ -1,27 +1,34 @@
+import type { Order } from "../types/order"
 
-//Filter by simple orders.
-const simpleOrders = allOrders.filter((order: any) => order.store.length !== 0)
-console.log("WooCommerceApi", "Simple orders length " + simpleOrders.length)
+interface FilterArgs {
+  orders: Order[]
+  seller: number
+}
 
-//Filter by Siempreverde.
-const svOrders = simpleOrders.filter((order: any) => order.store.id === 1)
-console.log("WooCommerceApi", "Siempreverde orders length " + svOrders.length)
+export function filterOrdersBySeller({orders, seller}: FilterArgs){
+  let aux = orders.filter((order: any) => order.store.id === seller)
+  
+  if(seller === 1){
+    const seres = orders.filter((order: any) => order.store.id === 2)
+    aux = aux.concat(seres)
+  } 
 
+  console.log("FilterOrdersByseller", "Filtered orders length " + aux.length)
+  
+  const filteredOrders = aux.map((order: any) => {
+    const aux = order.line_items.map((line: any) => ({
+      qty: line.quantity,
+      product_name: line.name,
+      price: line.price
+    }))
+    return {
+      order_number: order.id,
+      products: aux,
+      billing_full_name: order.billing.first_name + " " + order.billing.last_name,
+      order_total: order.total,
+      order_date: order.date_created
+    }
+  })
 
-//Build the order list for UI.
-const ordersSV = svOrders.map((order: any) => {
-  const aux = order.line_items.map((line: any) => ({
-    qty: line.quantity,
-    product_name: line.name,
-    price: line.price
-  }))
-  return {
-    order_number: order.id,
-    products: aux,
-    billing_full_name: order.billing.first_name + " " + order.billing.last_name,
-    order_total: order.total,
-    order_date: order.date_created
-  }
-})
-
-console.log("WooCommerceApi", "Returned orders length " + ordersSV.length)
+  return filteredOrders
+}
